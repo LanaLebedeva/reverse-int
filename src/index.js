5410727198d073805c97483e202e0d96c66a4427
lanalebedeva <batetnikova@mail.ru>
2022-06-22 13:28:12 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+      n *= (-1);
+  }
+  let result = '';
+  let numberString = n.toString();
+  for (let char of numberString) {
+      result = char + result
+  }
+  return parseInt(result);
 }
